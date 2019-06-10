26ba2c95efa026ff36d99ce167ed86d50b40ecf4
liang.gao <liang.gao@zhangmeng.com>
2019-06-10 20:53:55 +0800

    add annotation

--- src/com/company/lintcode/lintcode_1006.java
+++ src/com/company/lintcode/lintcode_1006.java
@@ -7,6 +7,15 @@ import java.lang.reflect.Array;
 import java.util.*;
 import java.util.List;
 
+/**
+ * 诸如discuss.lintcode.com这样的域名由各种子域名构成。最顶层是com，下一层是lintcode.com，
+ * 最底层是discuss.lintcode.com.当访问discuss.lintcode.com时，会隐式访问子域名lintcode.com和com.
+ * 现给出域名的访问计数格式为“空格 地址”。 示例：9001 discuss.lintcode.com.
+ * 给出计数列表cpdomains. 返回每个子域名（包含父域名）的访问次数（与输入格式相同，顺序任意）.
+ *
+ * 解决：
+ * 分割字符串（“.”需要转义），使用map，key是域名，value是计数。
+ */
 public class lintcode_1006 extends BaseDemo {
 
     @Override
