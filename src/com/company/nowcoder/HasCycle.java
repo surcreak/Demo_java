2294cc7e3b17ad8409f7e93ef4d3496ab80dc7c4
liang.gao <liang.gao@zhangmen.com>
2020-10-13 23:06:43 +0800

    add hascycle

---
+++ src/com/company/nowcoder/HasCycle.java
@@ -0,0 +1,15 @@
+package com.company.nowcoder;
+
+import com.company.base.BaseDemo;
+import com.company.base.ListNode;
+
+public class HasCycle extends BaseDemo {
+    @Override
+    public void solution() throws InterruptedException {
+
+    }
+
+    public boolean hasCycle(ListNode head) {
+        
+    }
+}
